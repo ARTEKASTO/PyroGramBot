7ddb71a7d3145a78edf40272923939c39cc40d9f
ARTEKASTO <86351729+ARTEKASTO@users.noreply.github.com>
2021-08-20 23:51:31 +0300

    Update sample_config.py

--- ateş robotu/sample_config.py
+++ ateş robotu/sample_config.py
@@ -42,7 +42,7 @@ class Config:
         "./DOWNLOADS/"
     )
     # get a Heroku API key from http://dashboard.heroku.com/account
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_API_KEY  =  işletim sistemi . çevre . get ( "HEROKU_API_KEY" , bbb46314-670f-4659-b714-0f7058ba5815 )
     # set this to your fork on GitHub (if you want)
     OFFICIAL_UPSTREAM_REPO = os.environ.get(
         "OFFICIAL_UPSTREAM_REPO",
@@ -58,7 +58,7 @@ class Config:
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
     # SuDo User
-    OWNER_ID = int(os.environ.get("OWNER_ID", "7351948"))
+    OWNER_ID  =  int ( os . environ . get ( "OWNER_ID" , "1776302393" )
     # Array to store users who are authorized to use the bot
     SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
     # the maximum number of 'selectable' messages in Telegram
